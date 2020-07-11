cffd3103103a6ac18c2fa740401dcf4fabae9e98
Vatsal <vatsalsinha001@gmail.com>
2020-07-11 17:28:21 +0530

    django-react-tensorflow

---
+++ whatimage/images/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    picture   = models.ImageField()
+    classfied = models.CharField(max_length=200, blank = True)
+    uploaded  = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return "Image classified at {}".format(self.uploaded.strftime('%Y-%m-%d %H:%M'))
+
+    def save(self, *args, **kwargs):
+        try:
+            print("success")
+        except:
+            print("classification failed")
+        super().save(*args, **kwargs)
